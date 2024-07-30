33e6f2995cf0ddee14294610922ac7b91bec5ae9
Eddie Zieleniewski <solesskidder1@gmail.com>
2024-07-30 00:09:14 -0400

    change auth import

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,10 @@
-import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_SECRET } from "$env/static/private"
+// import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_SECRET } from "$env/static/private"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
 
-const googleClientId = AUTH_GOOGLE_ID;
-const googleClientSecret = AUTH_GOOGLE_SECRET;
-const googleAuthSecret = AUTH_SECRET
+const googleClientId = process.env.AUTH_GOOGLE_ID;
+const googleClientSecret = process.env.AUTH_GOOGLE_SECRET;
+const googleAuthSecret = process.env.AUTH_SECRET
 export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
   const authOptions = {
     providers: [
