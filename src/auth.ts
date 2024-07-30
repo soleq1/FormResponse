03b94c8d44930264d0422c39b4bdc12615d44960
Eddie Zieleniewski <solesskidder1@gmail.com>
2024-07-29 23:58:00 -0400

    annoying vercel

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,19 @@
 import { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, AUTH_SECRET } from "$env/static/private"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
- 
 
+const googleClientId = AUTH_GOOGLE_ID;
+const googleClientSecret = AUTH_GOOGLE_SECRET;
+const googleAuthSecret = AUTH_SECRET
 export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
   const authOptions = {
     providers: [
       Google({
-        clientId: AUTH_GOOGLE_ID,
-        clientSecret: AUTH_GOOGLE_SECRET
+        clientId: googleClientId,
+        clientSecret: googleClientSecret
       })
     ],
-    secret: AUTH_SECRET,
+    secret: googleAuthSecret,
     trustHost: true
   }
   return authOptions
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
   import Footer from '$lib/components/footer.svelte';
 import Header from '$lib/components/header.svelte';
 
-
+  import { inject } from '@vercel/analytics'
     import '../app.css'
+    inject()
 </script>
+
 <Header />
 <slot />
 
